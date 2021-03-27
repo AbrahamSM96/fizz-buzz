845fd08e56f4f2341344399ae7c6eb2efea52e0e
Abraham Serrano <abrahamserrano@MacBook-Pro-de-Abraham-2.local>
2021-03-26 18:47:44 -0600

    add npm gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js',
+    filename: 'js/[name].[contenthash].js',
     assetModuleFilename: '[contenthash][ext][query]'
   },
   mode: 'production',
